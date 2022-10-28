521306b9dd8e69801167ae2fdf671d95e8e90609
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2022-10-28 21:08:55 +0300

    Add collectibles tracking

--- achievements.js
+++ achievements.js
@@ -53,65 +53,64 @@ var difficulties = [
   'hell on earth',
 ];
 
-function getMapFromText(text) {
-  let foundMap = '';
+/* Check text if it has one of given word list words. If a word is
+ * found, return it. */
+var getWordFoundInText = (text, wordList, defaultResult = '') => wordList.reduce((result, word) => {
+  if (text.includes(word)) {
+    result = word;
+  }
 
-  maps.forEach((map) => {
-    if (text.indexOf(map) > -1) {
-      foundMap = map;
-    }
-  });
+  return result;
+}, defaultResult);
 
-  console.assert(foundMap !== '', 'this text has no map: ' + text);
+var getData = () => {
+  const achievementElementRows = [...document.querySelectorAll('.achieveRow')];
 
-  return foundMap;
-}
+  const object = gamemodes.reduce((result, gamemode) => {
+    result[gamemode] = {};
+    result.collectibles = [];
 
-function getTextGamemode(text) {
-  let foundGamemode = '';
-
-  gamemodes.forEach((gamemode) => {
-    if (text.indexOf(gamemode) > -1) {
-      foundGamemode = gamemode;
-    }
-  });
-
-  return foundGamemode;
-}
-
-function test() {
-  const object = {};
-
-  gamemodes.forEach((gamemode) => {
-    object[gamemode] = {};
     difficulties.forEach((difficulty) => {
-      object[gamemode][difficulty] = [];
+      result[gamemode][difficulty] = [];
     });
-  });
 
-  const rows = [...document.querySelectorAll('.achieveRow')];
+    return result;
+  }, {});
 
-  return rows.reduce(function testing(acc, element) {
+  return achievementElementRows.reduce(function testing(acc, element) {
     const text = element.querySelector('h5').innerText.toLowerCase();
-    const gamemode = getTextGamemode(text);
+    const gamemode = getWordFoundInText(text, gamemodes, 'survival');
     const isAchieved = !!element.querySelector('.achieveUnlockTime');
-    ;
+    const map = getWordFoundInText(text, maps);
 
-    if (gamemode === '' || text.indexOf('map as') > -1) {
+    if (!map || isAchieved || text.includes('map as') ) {
       return acc;
     }
-    if (text.indexOf('hard or higher') > -1 && !isAchieved) {
-      acc[gamemode][difficulties[1]].push(getMapFromText(text));
-      acc[gamemode][difficulties[2]].push(getMapFromText(text));
-      acc[gamemode][difficulties[3]].push(getMapFromText(text));
-    } else {
-      difficulties.forEach((difficulty) => {
-        if (text.indexOf(difficulty) > -1 && !isAchieved) {
-          acc[gamemode][difficulty].push(getMapFromText(text));
-        }
-      });
+
+    const difficulty = getWordFoundInText(text, difficulties);
+    const isAboutCollecting = !!getWordFoundInText(text, ['collect', 'destroy']);
+
+    if (map && text.includes('hard or higher')) {
+      acc[gamemode][difficulties[1]].push(map);
+      acc[gamemode][difficulties[2]].push(map);
+      acc[gamemode][difficulties[3]].push(map);
+    } else if (map && difficulty) {
+      acc[gamemode][difficulty].push(map);
+    } else if (map && isAboutCollecting) {
+      acc.collectibles.push(map);
     }
 
     return acc;
   }, object);
+}
+
+var getJson = (playerName) => {
+  const data = {
+    name: playerName,
+    achievements: null,
+  }
+
+  data.achievements = getData();
+
+  return JSON.stringify(data, null, 4);
 }
