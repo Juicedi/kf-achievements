4dff1711991e1aae2e46214f1baf37812dc964f8
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2022-10-29 12:40:23 +0300

    Add buttons to change table content

--- src/main.js
+++ src/main.js
@@ -1,45 +1,56 @@
 /* global dataCollection maps */
 
-// TODO: pivot table so the least marks is first and most is last
+// TODO: Pivot table so the least marks is first and most is last
+// TODO: Sort rows based on how many marks they have
 // TODO: Make buttons that can change what is visible
 (function main() {
+  const title = document.getElementById('dataKey');
   const thead = document.querySelector('thead tr');
   const tbody = document.querySelector('tbody');
 
-  console.log(dataCollection);
+  const fillTable = (mode, difficulty) => {
+    function newCol({ text = '', element = 'TD' }) {
+      const col = document.createElement(element);
+      col.innerText = text;
+      return col;
+    }
 
-  maps.forEach((map) => {
-    const newRow = document.createElement('TR');
-    const mapName = document.createElement('TD');
+    maps.forEach((map) => {
+      const newRow = document.createElement('TR');
 
-    mapName.innerText = map;
-    newRow.appendChild(mapName);
+      newRow.appendChild(newCol({ text: map }));
+      dataCollection.forEach(() => newRow.appendChild(newCol({})));
 
-    let markCount = 0;
+      let markCount = 0;
 
-    document.getElementById('dataKey').innerText = 'Survival hard';
-
-    dataCollection.forEach((data) => {
-      const mark = document.createElement('TD');
-
-      if (data.achievements.survival.hard.includes(map)) {
+      dataCollection.forEach((data, index) => {
+        const datalist = mode === 'collectibles' ? data.achievements[mode] : data.achievements[mode][difficulty]
+        if (!datalist.includes(map)) return;
         markCount++;
-        mark.innerText = 'x';
-      } else {
-        mark.innerText = ' ';
-      }
+        newRow.children[index + 1].innerText = 'x';
+      });
 
-      newRow.appendChild(mark);
+      if (markCount > 0) {
+        tbody.appendChild(newRow);
+      }
     });
 
-    if (markCount > 0) {
-      tbody.appendChild(newRow);
-    }
-  });
+    thead.appendChild(newCol({ text: 'map', element: 'TH' }));
 
-  dataCollection.forEach((data) => {
-    const playerName = document.createElement('TH');
-    playerName.innerText = data.name;
-    thead.appendChild(playerName);
+    dataCollection.forEach((data) => {
+      thead.appendChild(newCol({ text: data.name, element: 'TH' }));
+    });
+  };
+
+  document.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const mode = event.currentTarget.dataset.mode;
+      const difficulty = mode === 'collectibles' ? '' : event.currentTarget.dataset.difficulty;
+      title.innerText = `${mode} ${difficulty}`;
+      title.classList.add('capitalize');
+      thead.innerHTML = '';
+      tbody.innerHTML = '';
+      fillTable(mode, difficulty);
+    });
   });
 }());
